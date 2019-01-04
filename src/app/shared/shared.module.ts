dcd4a681a1c519cb57884f953a8b4f187b011062
Jean Masgo <i1415905@continental.edu.pe>
2019-01-04 16:12:05 -0500

    listo para funcionalidad

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from "@angular/core";
+
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumsComponent } from './breadcrums/breadcrums.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+@NgModule({
+    declarations:[
+        HeaderComponent,
+        SidebarComponent,
+        NopagefoundComponent,
+        BreadcrumsComponent
+    ],
+    imports:[
+
+    ],
+    exports:[
+        HeaderComponent,
+        SidebarComponent,
+        NopagefoundComponent,
+        BreadcrumsComponent
+    ]
+})
+export class sharedModule{}
